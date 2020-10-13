50014baf774970f3cfe9c3b07b51451f04aa4147
jackywu <jacky.wucheng@gmail.com>
2020-10-13 09:34:18 +0800

    update

--- _posts/fix_image_path.sh
+++ _posts/fix_image_path.sh
@@ -1,2 +1,7 @@
 pattern="/Users/wucheng/codebase/blog/jackywu.github.io"
-sed -i "s/$pattern//g"  `grep -rl  '$pattern'  .`
+escaped_pattern="\/Users\/wucheng\/codebase\/blog\/jackywu.github.io"
+# sed -i "s/$escaped_pattern//g"  `grep -rl  '$pattern'  .`
+file_list=`grep -rl  "$pattern"  .|grep -v fix_image_path.sh`
+# echo $file_list
+# sed -i "s/$escaped_pattern//g" $file_list
+echo "s/$escaped_pattern//g" $file_list
